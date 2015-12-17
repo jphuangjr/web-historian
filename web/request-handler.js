407d30704f6b6a954be4caca9d3b639e27ff5df3
jphuangjr <jphuangjr@gmail.com>
2015-12-16 17:34:00 -0800

    WOrked on helper functions

--- web/request-handler.js
+++ web/request-handler.js
@@ -10,19 +10,20 @@ exports.handleRequest = function(req, res) {
     if (req.url === "/") {
       http.serveAssets(res, '/index.html', function(data) {
         res.writeHead(statusCode, http.headers);
-        archive.readListOfUrls();
+        archive.downloadUrls(["www.google.com", "yahoo.com"]);
         res.end(data);
       });
-    } else if (req.url === 'google.com') {
-      http.serveAssets(res, '/google.com', function(data) {
-        res.writeHead(statusCode, http.headers);
-        res.end(data);
-      });
-    } else {
-      statusCode = 404;
-      res.writeHead(statusCode, http.headers);
-      res.end();
     }
+  } else if (req.url === 'google.com') {
+    http.serveAssets(res, '/google.com', function(data) {
+      res.writeHead(statusCode, http.headers);
+      res.end(data);
+    });
+  } else {
+    statusCode = 404;
+    res.writeHead(statusCode, http.headers);
+    res.end();
   }
+
   // res.end(archive.paths.list);
 };
